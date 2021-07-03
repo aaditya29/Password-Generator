2bcde794fac1ab2acf9a89139a5b9498813f0f63
Aditya Mishra <mishraaaditya29@gmail.com>
2021-07-03 18:59:11 +0530

    Code Commit

--- password.py
+++ password.py
@@ -1,1 +1,11 @@
+import string
+from secrets import choice
+#The secrets module is used for generating cryptographically strong random numbers suitable
+#for managing data such as passwords, account authentication, security tokens, and related secrets.
+
+#In particular, secrets should be used in preference to the default pseudo-random number generator in the random module,
+#which is designed for modelling and simulation, not security or cryptography.
+
+# Here, secrets.choice(sequence) returns a randomly-chosen element from a non-empty sequence.
+
 
